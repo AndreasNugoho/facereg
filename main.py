16c1c00f15991b6e37f4465340416d1e07aaf936
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-03-02 14:36:05 +0700

    fix frame and color

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ camera = cv2.VideoCapture(0)
 
 def face_detection(frame):
     optimized_frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-    faces = face_ref.detectMultiScale(optimized_frame,scaleFactor=1.1)
+    faces = face_ref.detectMultiScale(optimized_frame,scaleFactor=1.1,minSize=(500,500), minNeighbors=3)
     return faces
 
 def drawer_box(frame):
     for x, y, w, h in face_detection(frame):
-        cv2.rectangle(frame,(x, y), (x + w, y + h),(0, 0, 255), 4)
+        cv2.rectangle(frame,(x, y), (x + w, y + h),(255, 0, 0), 4)
     
 def close_window():
     camera.release()
@@ -23,7 +23,7 @@ def main():
         drawer_box(frame)
         cv2.imshow("Facereg",frame)
         
-        if cv2.waitkey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             close_window()
     
 
