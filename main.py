986dd617c8b02114fd08d33afeffb2869578f3bc
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-03-02 14:10:19 +0700

    create frame

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ face_ref = cv2.CascadeClassifier("face_ref.xml")
 camera = cv2.VideoCapture(0)
 
 def face_detection():
+    faces = face_ref.detectMultiScale(frame,scaleFactor=1.1)
     pass
 
 def drawer_box():
+    for x,y,w,h in face_detection(frame):
+        cv2.rectangle(frame,(x,y), (x+w,y+h),(0,0,255),4)
+        
     pass
 
 def main():
