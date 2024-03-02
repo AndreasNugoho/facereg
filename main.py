5d32e168b52be20ab74224286eaff52707ca51cb
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-03-02 14:17:26 +0700

    create bw frame to optimized

--- main.py
+++ main.py
@@ -6,17 +6,26 @@ camera = cv2.VideoCapture(0)
 def face_detection():
     optimized_frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
     faces = face_ref.detectMultiScale(optimized_frame,scaleFactor=1.1)
-    pass
+    return faces
 
 def drawer_box():
     for x,y,w,h in face_detection(frame):
         cv2.rectangle(frame,(x,y), (x+w,y+h),(0,0,255),4)
-        
-    pass
+    
+def close_window():
+    camera.release()
+    cv2.destroyAllWindows()
+    exit()
 
 def main():
-    _,frame = camera.read()
-    pass
+    while True:
+        _,frame = camera.read()
+        drawer_box(frame)
+        cv2.imshow("Facereg",frame)
+        
+        if cv2.waitkey(1) & 0xFF == ord('q'):
+            break
+    
 
 if __name__ == "__main__":
     main()
